64769f79a59e4a2e35bd4b869a19e2faca7d1690
NuLeafAcademy <128169739+NuLeafAcademy@users.noreply.github.com>
2023-03-17 12:53:55 -0400

    Update auto-loan-calculator.js

--- auto-loan-calculator.js
+++ auto-loan-calculator.js
@@ -26,8 +26,8 @@ function newtonRaphsonMethod(principal, monthlyPayment, numPayments) {
   let deltaRate;
 
   do {
-    const f = principal * rate * Math.pow(1 + rate, numPayments) / (Math.pow(1 + rate, numPayments) - 1) - monthlyPayment;
-    const f_prime = principal * (Math.pow(1 + rate, numPayments) * (numPayments * rate - numPayments + 1) + rate) / (Math.pow(1 + rate, 2 * numPayments) - 2 * Math.pow(1 + rate, numPayments) + 1);
+    const f = principal * rate * (1 - Math.pow(1 + rate, -numPayments)) - monthlyPayment;
+    const f_prime = principal * (Math.pow(1 + rate, -numPayments) * (numPayments * rate - numPayments + 1) + rate);
     deltaRate = -f / f_prime;
     rate += deltaRate;
   } while (Math.abs(deltaRate) > epsilon);
